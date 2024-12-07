ad61150b6467a4ae366608cb23c8d49f0945248f
ndegwah <gibsonndegwa12@gmail.com>
2024-12-07 23:05:37 +0300

    hope not error
    please please

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -116,7 +116,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+import os
+
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+
+# Specify the directories for additional static files
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # For project-wide static files
+]
+
+# Static root for collecting static files during deployment
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
