7a724268b4eea3eb57a8616a3520420971f0d307
ndegwah <gibsonndegwa12@gmail.com>
2024-12-08 12:22:02 +0300

    UHBRYUYHB
      7RY7RGY7RGY

---
+++ django_blog/blog/urls.py
@@ -0,0 +1,8 @@
+from django.contrib.auth import views as auth_views 
+from .import views
+
+urlpatterns = [
+  
+
+
+]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
