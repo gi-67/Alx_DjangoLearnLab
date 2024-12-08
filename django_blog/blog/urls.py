76f761fe8e894b14d974ba75d36b8997d1ec6838
ndegwah <gibsonndegwa12@gmail.com>
2024-12-08 14:28:23 +0300

    dgdzh
    hbdhdch

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,8 +1,11 @@
-from django.contrib.auth import views as auth_views 
-from .import views
+from django.contrib.auth import views as auth_views
+from . import views
 
 urlpatterns = [
-  
-
-
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
+    path('register/', views.register, name='register'),  # Custom registration view
+    path('profile/', views.profile, name='profile'),  # Profile management view
 ]
+
+
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -3,3 +3,17 @@ from django.contrib import messages
 from .forms import UserRegisterForm
 from django.contrib.auth.decorators import login_required
 
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}! You can now log in.')
+            return redirect('login')
+        else:
+             form = UserRegisterForm()
+        return render(request,'registration/register.html', {'form': form})
+    @login_required
+    def profile(request):
+        return render(request,'registration/profile.html')
